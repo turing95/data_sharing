6f58f1c0a46eb75d90cac33eaa292d95f919533d
marcoturetta <marco.turetta@blinklastmile.com>
2023-11-23 20:00:54 +0100

    chore

--- web_app/views/__init__.py
+++ web_app/views/__init__.py
@@ -1,3 +1,3 @@
 from web_app.views.receiver.spaces import SpacesView
 from web_app.views.receiver.space_create import SpaceFormView
-from web_app.views.receiver.space_detail import SpaceDetailFormView
+from web_app.views.sender.space_detail import SpaceDetailFormView
--- web_app/views/sender/space_detail.py
+++ web_app/views/sender/space_detail.py
@@ -2,25 +2,23 @@ from web_app.forms import SpaceDetailForm
 from django.views.generic.edit import FormView
 from web_app.models import Space
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django import forms
 
 
-class SpaceDetailFormView(FormView,LoginRequiredMixin):
-    template_name = "receiver/space_detail.html"
+class SpaceDetailFormView(FormView):
+    template_name = "sender/space_detail.html"
     form_class = SpaceDetailForm
     success_url = reverse_lazy('spaces')
 
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
         return super().form_valid(form)
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        space = Space.objects.get(uuid=self.kwargs['space_uuid'])  # Retrieve the object from the database
+        space = Space.objects.get(uuid=self.kwargs['space_uuid'])
 
         for index, space_req in enumerate(space.requests.all()):
-            form.fields[f'file_{index}'] = forms.FileField()  # Dynamically adding file fields
+            form.fields[f'file_{index}'] = forms.FileField()
 
         return form
