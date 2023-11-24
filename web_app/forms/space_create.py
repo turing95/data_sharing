c2eb359253898168d23bf03d5469abd9f1ed934d
marcoturetta <marco.turetta@blinklastmile.com>
2023-11-24 12:21:20 +0100

    chore

--- web_app/forms/space_create.py
+++ web_app/forms/space_create.py
@@ -27,10 +27,11 @@ class SpaceForm(ModelForm):
         label='Emails',
         required=False  # Set to True if emails are mandatory
     )
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}))
 
     class Meta:
         model = Space
-        fields = ['name','is_public']
+        fields = ['name', 'is_public']
 
     def clean_emails(self):
         # Custom clean method for emails field
