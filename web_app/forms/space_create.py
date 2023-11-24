2472dec60ccd40b3c682cdd8c55119fe784b1f35
marcoturetta <marco.turetta@blinklastmile.com>
2023-11-24 13:26:43 +0100

    chore

--- web_app/forms/space_create.py
+++ web_app/forms/space_create.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm
-from web_app.models import Space
+from django.forms import ModelForm,DateTimeInput,inlineformset_factory
+from web_app.models import Space,Request
 from django import forms
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
@@ -23,11 +23,13 @@ class CommaSeparatedEmailField(forms.CharField):
 
 class SpaceForm(ModelForm):
     senders_emails = CommaSeparatedEmailField(
-        widget=forms.TextInput(attrs={'placeholder': 'Enter emails separated by commas', 'class': 'email-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Enter emails separated by commas',
+                                      'class': 'email-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}),
         label='Emails',
         required=False  # Set to True if emails are mandatory
     )
-    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter name of the space','class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter name of the space',
+                                                         'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}))
     is_active = forms.BooleanField(
         widget=forms.CheckboxInput(attrs={
             'class': 'form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out'
@@ -35,11 +37,24 @@ class SpaceForm(ModelForm):
         required=False,
         label='Publish'
     )
+
     class Meta:
         model = Space
-        fields = ['name', 'is_public','is_active']
+        fields = ['name', 'is_public', 'is_active']
 
     def clean_emails(self):
         # Custom clean method for emails field
         emails = self.cleaned_data.get('senders_emails', [])
         return emails
+
+
+class RequestForm(ModelForm):
+    class Meta:
+        model = Request
+        fields = ['destination', 'deadline', 'number_of_files']
+        widgets = {
+            'deadline': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+
+RequestFormSet = inlineformset_factory(Space, Request, form=RequestForm, extra=1, can_delete=True)
--- web_app/views/receiver/space_create.py
+++ web_app/views/receiver/space_create.py
@@ -1,4 +1,4 @@
-from web_app.forms import SpaceForm
+from web_app.forms import SpaceForm,RequestFormSet
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -10,8 +10,19 @@ class SpaceFormView(FormView, LoginRequiredMixin):
     form_class = SpaceForm
     success_url = reverse_lazy('spaces')
 
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        if self.request.POST:
+            data['requests'] = RequestFormSet(self.request.POST)
+        else:
+            data['requests'] = RequestFormSet()
+        return data
+
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
+        context = self.get_context_data()
+        requests = context['requests']
+
         space_instance = form.save(commit=False)
         space_instance.user = self.request.user
         space_instance.save()
@@ -19,4 +30,8 @@ class SpaceFormView(FormView, LoginRequiredMixin):
         emails = form.cleaned_data['senders_emails']
         for email in emails:
             Sender.objects.create(email=email, space=space_instance)
+
+        if requests.is_valid():
+            requests.instance = space_instance
+            requests.save()
         return super().form_valid(form)
