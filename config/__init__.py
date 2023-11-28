cbbd4b3e1cf0466776f94a861feb6d5d3a759bdf
marcoturetta <marco.turetta@blinklastmile.com>
2023-11-28 12:37:46 +0100

    chore

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,9 @@
 from config._secret import *
+
+
+def get_js_config():
+    return {
+        'googleApiKey': GOOGLE_API_KEY,
+        'googleClientId': GOOGLE_CLIENT_ID,
+        'googleAppId': GOOGLE_APP_ID
+    }
