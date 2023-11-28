2a8c69ce6c5efc67f9fd1ca2a71b9ffaeb349034
marcoturetta <marco.turetta@blinklastmile.com>
2023-11-28 10:04:05 +0100

    chore

--- web_app/forms/login.py
+++ web_app/forms/login.py
@@ -1,4 +1,7 @@
 from allauth.account.forms import LoginForm as AllauthLoginForm
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 from web_app.forms.css_classes import text_input
 
 
@@ -6,5 +9,10 @@ class LoginForm(AllauthLoginForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        reset_url = reverse("account_reset_password")
         self.fields["password"].widget.attrs["class"] = text_input
+        self.fields["password"].help_text = mark_safe(
+            f'<p class="text-sm">Forgot your password? <a href="{reset_url}" class="text-blue-600 hover:text-blue-600 underline">Reset</a></p>'
+        )
+
         self.fields["login"].widget.attrs["class"] = text_input
--- web_app/views/receiver/password_reset.py
+++ web_app/views/receiver/password_reset.py
@@ -1,9 +1,5 @@
 from allauth.account.views import PasswordResetView as AllauthPasswordResetView
-from django.urls import reverse_lazy
-from web_app.forms.login import LoginForm
 
 
-class LoginView(AllauthPasswordResetView):
-    form_class = LoginForm
-    success_url = reverse_lazy('spaces')
-    template_name = 'receiver/public/login.html'
+class PasswordResetView(AllauthPasswordResetView):
+    template_name = 'receiver/public/password_reset.html'
