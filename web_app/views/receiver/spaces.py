9c0891bc6819a67b53739206e2acfa91331f5bf6
GiulioCantoro <giulio.cantoro@gmail.com>
2023-11-29 17:55:39 +0100

    feat: styling of spaces page

--- web_app/views/receiver/spaces.py
+++ web_app/views/receiver/spaces.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView 
 from web_app.models import Space
 from django.contrib.auth.mixins import LoginRequiredMixin
 
