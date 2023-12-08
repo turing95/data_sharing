d10b084eb695a90ad53b93c500923b9be1ddce8d
marcoturetta <marco.turetta@blinklastmile.com>
2023-12-08 16:15:28 +0100

    chore

--- web_app/views/__init__.py
+++ web_app/views/__init__.py
@@ -1,9 +1,9 @@
 from web_app.views.receiver.space.list import SpacesView
 from web_app.views.receiver.space.create import SpaceFormView
 from web_app.views.receiver.space.delete import delete_space
-from web_app.views.receiver.signup import SignupView
-from web_app.views.receiver.login import LoginView
-from web_app.views.receiver.reset_password import PasswordResetView
+from web_app.views.receiver.authentication.signup import SignupView
+from web_app.views.receiver.authentication.login import LoginView
+from web_app.views.receiver.authentication.reset_password import PasswordResetView
 from web_app.views.sender.space_detail import SpaceDetailFormView as SpaceDetailFormViewSender
 from web_app.views.receiver.space.detail import SpaceDetailFormView as SpaceDetailFormViewReceiver
 from web_app.views.receiver.terms_of_service import TermsOfServiceView
