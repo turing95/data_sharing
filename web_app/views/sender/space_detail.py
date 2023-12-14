dc0372b8b8114cc9bd09fe4bbb8a6506c935b106
GiulioCantoro <giulio.cantoro@gmail.com>
2023-12-14 19:04:50 +0100

    fix: canelled last commit about link to sender space detail deactivated

--- web_app/views/sender/space_detail.py
+++ web_app/views/sender/space_detail.py
@@ -76,7 +76,7 @@ class SpaceDetailView(TemplateView):
 
             if sender_id is not None:
                 try:
-                    self._sender = Sender.objects.get(pk=sender_id)
+                    self._sender = Sender.objects.get(pk=sender_id, is_active=True)
                 except Sender.DoesNotExist:
                     raise Http404(f"Sender with id '{sender_id}' not found")
 
