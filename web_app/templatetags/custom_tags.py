40c0eb132e38101c67152eab1b7910762cfff739
GiulioCantoro <giulio.cantoro@gmail.com>
2023-12-15 19:07:43 +0100

    feat: tag to render toggle NON FUNZIONANTE

--- web_app/templatetags/custom_tags.py
+++ web_app/templatetags/custom_tags.py
@@ -22,3 +22,24 @@ def get_list_of_upload_events_per_request(sender, upload_request):
                                         event_type=SenderEvent.EventType.FILE_UPLOADED)
 
     return events
+
+
+@register.inclusion_tag("forms/widgets/toggle.html")
+def build_toggle(**kwargs):
+    """ to render a toggle button in a template"""
+    # Define default values for the keys
+    defaults = {
+        'color_on': 'blue',
+        'color_off': 'gray',
+        'label_on': '',
+        'label_off': '',
+        'label_colored': False,
+        'soft_off_label': False,
+        'label_wrap': False,
+        'label_wrap_mono': False
+    }
+
+    # Update defaults with provided kwargs
+    defaults.update(kwargs)
+
+    return defaults
