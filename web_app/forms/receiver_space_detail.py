f6a731c9e975717ba9ab7818f559c409fe74ee6d
marcoturetta <marco.turetta@blinklastmile.com>
2023-12-15 12:49:10 +0100

    fix: file naming formula

--- web_app/forms/receiver_space_detail.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from web_app.models import Space
-
-
-class SpaceDetailForm(ModelForm):
-    class Meta:
-        model = Space
-        fields = ['title']  # Include only the editable field
