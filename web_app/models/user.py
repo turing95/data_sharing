36b0d2b8c930cbac7c9118ea6771c45065ab546b
marcoturetta <marco.turetta@blinklastmile.com>
2023-12-17 13:40:39 +0100

    feat: user flow to add google account

--- web_app/models/user.py
+++ web_app/models/user.py
@@ -10,25 +10,32 @@ class CustomUser(User):
     class Meta:
         proxy = True
 
+    def google_account(self):
+        try:
+            return SocialAccount.objects.get(user=self, provider='google')
+        except SocialAccount.DoesNotExist:
+            return None
+
     def refresh_google_token(self):
         try:
-            social_account = SocialAccount.objects.get(user=self, provider='google')
-            token = SocialToken.objects.get(account=social_account)
-            if arrow.get(token.expires_at) < arrow.utcnow():
-                credentials = Credentials(
-                    token=token.token,
-                    refresh_token=token.token_secret,
-                    token_uri='https://accounts.google.com/o/oauth2/token',
-                    client_id=config.GOOGLE_CLIENT_ID,
-                    client_secret=config.GOOGLE_CLIENT_SECRET
-                )
-                credentials.refresh(Request())
-                token.token = credentials.token
-                token.expires_at = credentials.expiry
-                token.save()  # Update the token in the database
+            social_account = self.google_account()
+            if social_account is not None:
+                token = SocialToken.objects.get(account=social_account)
+                if arrow.get(token.expires_at) < arrow.utcnow():
+                    credentials = Credentials(
+                        token=token.token,
+                        refresh_token=token.token_secret,
+                        token_uri='https://accounts.google.com/o/oauth2/token',
+                        client_id=config.GOOGLE_CLIENT_ID,
+                        client_secret=config.GOOGLE_CLIENT_SECRET
+                    )
+                    credentials.refresh(Request())
+                    token.token = credentials.token
+                    token.expires_at = credentials.expiry
+                    token.save()  # Update the token in the database
 
-            return token
-        except (SocialAccount.DoesNotExist, SocialToken.DoesNotExist):
+                return token
+        except SocialToken.DoesNotExist:
             # Handle the case where the user does not have a Google social account
             # or the token does not exist
             return None
