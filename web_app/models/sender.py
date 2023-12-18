d6b3eaf5406529fde5c14435cda586611c1e750e
marcoturetta <marco.turetta@blinklastmile.com>
2023-12-18 13:49:32 +0100

    feat notifications email

--- web_app/models/sender.py
+++ web_app/models/sender.py
@@ -7,7 +7,3 @@ from web_app.models import BaseModel, ActiveModel
 class Sender(BaseModel, ActiveModel):
     email = models.CharField(max_length=50)
     space = models.ForeignKey('Space', on_delete=models.CASCADE, related_name='senders')
-
-    @property
-    def requests(self):
-        return self.space.requests.all()
