c744fcc84acd270c01c62e3dc81d2668c1796017
marcoturetta <marco.turetta@blinklastmile.com>
2023-12-20 07:51:18 +0100

    fix: request query sender

--- web_app/forms/file.py
+++ web_app/forms/file.py
@@ -30,7 +30,7 @@ class FileForm(Form):
         self.request_index = kwargs.pop('request_index')
         self.space = kwargs.pop('space')
         super().__init__(**kwargs)
-        upload_request = self.space.requests.all()[self.request_index]
+        upload_request = self.space.requests.filter(is_deleted=False)[self.request_index]
         self.fields['request_uuid'].initial = upload_request.pk
         self.upload_request = upload_request
 
