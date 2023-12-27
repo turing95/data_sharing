574c81ceb92f892fe302438123d8b1ad6c1ec3b8
marcoturetta <marco.turetta@blinklastmile.com>
2023-12-27 11:08:28 +0100

    fix

--- web_app/views/api/space.py
+++ web_app/views/api/space.py
@@ -29,6 +29,7 @@ def upload_events(request, space_uuid):
     events = space.upload_events
     return render(request, 'private/space/detail/components/history_table.html', {'events': events})
 
+
 @require_GET
 def sender_request_files(request, space_uuid):
     space = Space.objects.get(pk=space_uuid)
@@ -38,28 +39,25 @@ def sender_request_files(request, space_uuid):
     events = events.filter(request__uuid=request.GET.get('request_uuid'),
                            sender__uuid=request.GET.get('sender_uuid'))
 
-    
-        # Get the additional parameters from the request
+    # Get the additional parameters from the request
     hide_sender = request.GET.get('hide_sender', 'False') == 'True'
     hide_request = request.GET.get('hide_request', 'False') == 'True'
 
     return render(request, 'private/space/detail/components/history_table.html',
                   {'events': events, 'hide_sender': hide_sender, 'hide_request': hide_request})
 
-    
+
 @require_GET
 def sender_request_modal(request, space_uuid, request_uuid, sender_uuid):
     space = get_object_or_404(Space, pk=space_uuid)
-    upload_request = get_object_or_404(UploadRequest, pk=request_uuid, space=space)  # Ensures request is part of the space
+    upload_request = get_object_or_404(UploadRequest, pk=request_uuid,
+                                       space=space)  # Ensures request is part of the space
     sender = get_object_or_404(Sender, pk=sender_uuid, space=space)  # Replace 'Sender' with the correct model name
-    
-    space = Space.objects.get(pk=space_uuid)
     events = space.upload_events
 
     # Filter the events by request and sender
     events = events.filter(request__uuid=request_uuid,
                            sender__uuid=sender_uuid)
 
-
-    return render(request, 'private/space/detail/components/sender_request_files_modal.html', 
+    return render(request, 'private/space/detail/components/sender_request_files_modal.html',
                   {'space': space, 'req': upload_request, 'sender': sender, 'events': events})
