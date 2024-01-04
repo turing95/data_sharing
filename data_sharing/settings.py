a5889298e65c81eba50bbcda3e44729910b36c31
GiulioCantoro <giulio.cantoro@gmail.com>
2024-01-04 13:39:04 +0100

    feat: url of logo goes to homepage, login redirects to spaces

--- data_sharing/settings.py
+++ data_sharing/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 SOCIALACCOUNT_STORE_TOKENS = True
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'spaces'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
