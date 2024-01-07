a23b0e92432dea56521d572f29112f8c3bb97a75
GiulioCantoro <giulio.cantoro@gmail.com>
2024-01-07 16:48:29 +0100

    feat: navbar behavior on home page (menu links)

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,9 +1,13 @@
 import {initMessageBar} from "./utils/messageBar.js";
 import { initSubmitButtons } from './utils/submitButton.js';
+import { initSmoothScrolling, adjustScrollPositionOnPageLoad } from './utils/smoothScrolling.js';
+import { initNav } from './utils/navbar.js';
+
 document.addEventListener('DOMContentLoaded', function() {
         initSubmitButtons();
         initMessageBar();
-
-
-
+        initSmoothScrolling();
+        adjustScrollPositionOnPageLoad()
+        initNav();
 });
+
