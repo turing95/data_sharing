c2b2d835b65fa8c9fc294f9a3575641d9e99524c
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-08 16:41:30 +0100

    chore: semplification

--- web_app/context_processors.py
+++ web_app/context_processors.py
@@ -1,27 +1,11 @@
 import config
 
 
-def private_area_context(request):
-    # Set private_area to True only if the user is authenticated
-    return {'private_area': request.user.is_authenticated, 'config_data': config.get_js_config()}
-
-def account_pages_context(request):
-    # Check if '/accounts/' is in the first part of the URL path
-    if 'accounts' in request.path.split('/')[1]:
-        return {'account_page': True}
-    else:
-        return {'account_page': False}
-
-def upload_pages_context(request):
-    # Check if '/accounts/' is in the first part of the URL path
-    if 'upload' in request.path.split('/')[1]:
-        return {'upload_page': True}
-    else:
-        return {'upload_page': False}
-    
-def spaces_pages_context(request):
-    # Check if '/accounts/' is in the first part of the URL path
-    if 'spaces' in request.path.split('/')[1]:
-        return {'spaces_page': True}
-    else:
-        return {'spaces_page': False}
+def custom_context(request):
+    url_name = request.resolver_match.url_name
+    context = {}
+    if url_name is not None:
+        context['sender_area'] = True if 'sender' in url_name else False
+        context['generic_area'] = True if 'generic' in url_name else False
+    context['config_data'] = config.get_js_config()
+    return context
