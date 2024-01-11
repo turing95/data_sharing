3aa8202cd80ae0759bb1274f5e8adf38ff2e464d
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-11 07:37:48 +0100

    fix: file type position

--- web_app/models/__init__.py
+++ web_app/models/__init__.py
@@ -1,8 +1,8 @@
 from web_app.models.base import BaseModel, ActiveModel, PolymorphicRelationModel, DeleteModel
 from web_app.models.space import Space
 from web_app.models.sender import Sender
-from web_app.models.request import UploadRequest, UploadRequestFileType,FileType
+from web_app.models.request import UploadRequest, UploadRequestFileType
 from web_app.models.destination import GoogleDrive,GenericDestination
 from web_app.models.event import SenderEvent
-from web_app.models.file import File
+from web_app.models.file import File,FileType
 from web_app.models.user import CustomUser
--- web_app/models/request.py
+++ web_app/models/request.py
@@ -68,27 +68,6 @@ class UploadRequest(BaseModel, DeleteModel):
         return file_name
 
 
-class FileType(BaseModel):
-    group = models.BooleanField(default=False)
-    slug = models.CharField(max_length=50, unique=True)
-    extension = models.CharField(max_length=50, null=True, blank=True)
-    upload_requests = models.ManyToManyField('UploadRequest', through='UploadRequestFileType')
-    group_type = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL,
-                                   limit_choices_to={"group": True})
-
-    @property
-    def extensions(self):
-        return [file_type.extension for file_type in self.filetype_set.all()]
-
-    @property
-    def formatted_extensions(self):
-        return [file_type.formatted_extension for file_type in self.filetype_set.all()]
-
-    @property
-    def formatted_extension(self):
-        if self.group is False:
-            return f".{self.extension}"
-        return None
 
 
 class UploadRequestFileType(BaseModel):
