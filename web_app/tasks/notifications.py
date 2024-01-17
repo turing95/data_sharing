0fc6e40fb2007c531aa406a9cf94a04a55c439fa
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-17 08:15:28 +0100

    feat: emails

--- web_app/tasks/notifications.py
+++ web_app/tasks/notifications.py
@@ -1,8 +1,8 @@
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, get_connection
 from django.template.loader import render_to_string
 from utils.emails import html_to_text
 from web_app import celery_app as app
-
+from django.conf import settings
 
 @app.task
 def sender_invite(sender_pk):
@@ -16,21 +16,28 @@ def sender_invite(sender_pk):
     context['calendar_url'] = calendar_url
 
     email_html = render_to_string('emails/sender_invite.html', context)
-    from_email = 'marco1491995@gmail.com'  # TODO: change to config
+    from_email = settings.NO_REPLY_EMAIL
+    with get_connection(
+            host=settings.RESEND_SMTP_HOST,
+            port=settings.RESEND_SMTP_PORT,
+            username=settings.RESEND_SMTP_USERNAME,
+            password=settings.RESEND_API_KEY,
+            use_tls=True,
+    ) as connection:
+        msg = EmailMultiAlternatives(
+            subject='Invite to space',
+            body=html_to_text(email_html),
+            from_email=from_email,
+            to=[sender.email],
+            reply_to=[from_email],
+            connection=connection,
+            headers={'Return-Path': from_email}
+        )
+        msg.attach_alternative(email_html, 'text/html')
+        if ics_content is not None:
+            msg.attach('event.ics', ics_content, 'text/calendar')
 
-    msg = EmailMultiAlternatives(
-        subject='Invite to space',
-        body=html_to_text(email_html),
-        from_email=from_email,
-        to=[sender.email],
-        reply_to=[from_email],
-        headers={'Return-Path': from_email}
-    )
-    msg.attach_alternative(email_html, 'text/html')
-    if ics_content is not None:
-        msg.attach('event.ics', ics_content, 'text/calendar')
-
-    msg.send()
+        msg.send()
 
 
 @app.task
@@ -41,16 +48,23 @@ def notify_deadline(sender_pk):
         'sender': sender,
     }
     email_html = render_to_string('emails/deadline_notification.html', context)
-    from_email = 'marco1491995@gmail.com'  # TODO: change to config
+    from_email = settings.NO_REPLY_EMAIL
+    with get_connection(
+            host=settings.RESEND_SMTP_HOST,
+            port=settings.RESEND_SMTP_PORT,
+            username=settings.RESEND_SMTP_USERNAME,
+            password=settings.RESEND_API_KEY,
+            use_tls=True,
+    ) as connection:
+        msg = EmailMultiAlternatives(
+            subject='Deadline notification',
+            body=html_to_text(email_html),
+            from_email=from_email,
+            to=[sender.email],
+            reply_to=[from_email],
+            connection=connection,
+            headers={'Return-Path': from_email}
+        )
+        msg.attach_alternative(email_html, 'text/html')
 
-    msg = EmailMultiAlternatives(
-        subject='Deadline notification',
-        body=html_to_text(email_html),
-        from_email=from_email,
-        to=[sender.email],
-        reply_to=[from_email],
-        headers={'Return-Path': from_email}
-    )
-    msg.attach_alternative(email_html, 'text/html')
-
-    msg.send()
+        msg.send()
