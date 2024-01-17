2c20a6a4b76060f2bc18e222a511f9aaac9ceaee
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-17 15:27:58 +0100

    fix: toggle

--- web_app/views/api/sender.py
+++ web_app/views/api/sender.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.decorators.http import require_POST
 
+from web_app.forms.widgets import ToggleWidget
 from web_app.models import Sender
 
 
@@ -14,8 +16,19 @@ def toggle_sender_active(request, sender_uuid):
     sender.save()
     return render(
         request,
-        'private/space/detail/components/sender_row.html',
-        {'sender': sender, 'space': sender.space}
+        "forms/widgets/toggle.html",
+        ToggleWidget(color_on="green",
+                     color_off="orange",
+                     toggle_color_on="marian-blue",
+                     toggle_color_off="gray",
+                     label_on="active",
+                     label_off="inactive",
+                     soft_off_label=False,
+                     label_colored=True,
+                     label_wrap=True).get_context('sender_toggle', sender.is_active,
+                                           {'hx-post': reverse('toggle_sender_active',
+                                                               kwargs={'sender_uuid': sender.pk}),
+                                            'hx-trigger': 'click', 'hx-swap': 'outerHTML','hx-target':'closest .cursor-pointer'})
     )
 
 
