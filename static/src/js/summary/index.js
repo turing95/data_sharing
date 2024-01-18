74fb5567f7fdad32eae013265d2ceb032c495e50
GiulioCantoro <giulio.cantoro@gmail.com>
2024-01-18 18:03:43 +0100

    casino

--- static/src/js/summary/index.js
+++ static/src/js/summary/index.js
@@ -9,3 +9,34 @@ document.body.addEventListener('htmx:afterSwap', function(event) {
             }
         }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('[id^="sender_actions_dropdown_button"]');
+
+    elements.forEach((element, index) => {
+        element.addEventListener('click', () => {
+            // Constructing the ID for initModal function
+            const modalId = `sender_actions_dropdown_${index}`;
+            initModal2(modalId);
+        });
+    });
+});
+
+function initModal2(id) {
+    const modalElement = document.getElementById(id);
+    if (!modalElement) {
+        console.error('Modal element not found');
+        return;
+    }
+    const modal = new Modal(modalElement, {
+        onHide: () => {
+            modalElement.remove();
+        },
+        backdropClasses:
+        'fixed inset-0 z-[5]',
+    });
+
+    // show the modal upon initialization
+    modal.show();
+}
