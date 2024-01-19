965d44d4eec0f3541c16c7f85178bbd3f2a4fc26
GiulioCantoro <giulio.cantoro@gmail.com>
2024-01-19 12:39:58 +0100

    chore: fixed JS

--- static/src/js/summary/index.js
+++ static/src/js/summary/index.js
@@ -1,4 +1,5 @@
 import {initModal} from "./files_modal/index.js";
+import {initSenderDropdowns} from "./senderActionsDropdown.js";
 
 document.body.addEventListener('htmx:afterSwap', function(event) {
 
@@ -10,42 +11,9 @@ document.body.addEventListener('htmx:afterSwap', function(event) {
         }
 });
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('[id^="sender_actions_dropdown_button"]');
-    let currentOpenDropdown = null;
-    const scrollableContainer = document.getElementById('scrollable-table-container');
-
-    elements.forEach((element) => {
-        element.addEventListener('click', () => {
-            // Close the currently open dropdown, if any
-            if (currentOpenDropdown && currentOpenDropdown !== element) {
-                scrollableContainer.classList.remove('overflow-visible');
-                scrollableContainer.classList.add('overflow-x-auto');
-            }
-
-            // Toggle the clicked dropdown
-            if (currentOpenDropdown !== element) {
-                // Open the clicked dropdown
-                scrollableContainer.classList.add('overflow-visible');
-                scrollableContainer.classList.remove('overflow-x-auto');
-                currentOpenDropdown = element; // Update the currently open dropdown
-            } else {
-                // Close the clicked dropdown
-                scrollableContainer.classList.remove('overflow-visible');
-                scrollableContainer.classList.add('overflow-x-auto');
-                currentOpenDropdown = null;
-            }
-        });
-    });
-
-    document.addEventListener('click', (event) => {
-        // Check if the click is outside the current open dropdown
-        if (currentOpenDropdown && !currentOpenDropdown.contains(event.target)) {
-            // Close the dropdown
-            scrollableContainer.classList.remove('overflow-visible');
-            scrollableContainer.classList.add('overflow-x-auto');
-            currentOpenDropdown = null;
-        }
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    initSenderDropdowns()
 });
+
+
+
