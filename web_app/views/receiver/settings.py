cb2f03a679644ca0772979452b2975f02c3bcb39
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-19 10:35:29 +0100

    fix: settings view

--- web_app/views/receiver/settings.py
+++ web_app/views/receiver/settings.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from web_app.mixins import SubscriptionMixin
 
 
-class SettingsView(LoginRequiredMixin, TemplateView):
+class SettingsView(LoginRequiredMixin,SubscriptionMixin, TemplateView):
     template_name = "private/settings.html"
 
