73da00539c609cd53d65c207e5447e2bd9bc04f6
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-19 17:45:09 +0100

    refactor: form js

--- static/src/js/space_create/index.js
+++ static/src/js/space_create/index.js
@@ -1,6 +1,5 @@
-import {initEmailInput} from "./emailInputWidget.js";
 import {initRequestForms,toggleRename,handleTagDropdownChange,addFileTypeTag, toggleFileTypeRestrict,handleAuthClick } from "./request/index.js";
-import {toggleDeadlineNotify, setNotificationDatetime, toggleDeadlineSettings} from './eventHandlers.js';
+import {initForm} from "./form.js";
 
 window.toggleRename = toggleRename;
 window.toggleFileTypeRestrict= toggleFileTypeRestrict
@@ -8,35 +7,8 @@ window.handleTagDropdownChange = handleTagDropdownChange;
 window.handleAuthClick = handleAuthClick;
 window.addFileTypeTag = addFileTypeTag;
 document.addEventListener('DOMContentLoaded', function() {
+    initForm();
     initRequestForms();
-    initEmailInput();
-
-    let notifyDeadlineToggle = document.querySelector('#id_notify_deadline');
-    if (notifyDeadlineToggle) {
-        notifyDeadlineToggle.addEventListener('change', toggleDeadlineNotify);
-        toggleDeadlineNotify(notifyDeadlineToggle)
-    }
-
-    // Select the inputs
-    const deadlineInput = document.getElementById('id_deadline');
-    const noticeDaysInput = document.getElementById('id_deadline_notice_days');
-    const noticeHoursInput = document.getElementById('id_deadline_notice_hours');
-
-    // Add event listeners
-    if (deadlineInput) {
-        deadlineInput.addEventListener('change', setNotificationDatetime);
-        deadlineInput.addEventListener('change', toggleDeadlineSettings);
-        toggleDeadlineSettings(deadlineInput);
-    }
-    if (noticeDaysInput) {
-        noticeDaysInput.addEventListener('change', setNotificationDatetime);
-    }
-    if (noticeHoursInput) {
-        noticeHoursInput.addEventListener('change', setNotificationDatetime);
-    }
-    
-
-
 });
 
 //handle enter button behavior
