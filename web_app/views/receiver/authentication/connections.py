505195c5cb1ccc8c545770d0e550e030bda38314
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-22 10:16:03 +0100

    fix: connections

--- web_app/views/receiver/authentication/connections.py
+++ web_app/views/receiver/authentication/connections.py
@@ -1,6 +1,11 @@
 from allauth.socialaccount.views import ConnectionsView as AllAuthConnectionsView
+from django.http import Http404
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
 
 class ConnectionsView(AllAuthConnectionsView):
     success_url = reverse_lazy("account_settings")
+
+    def get(self, request, *args, **kwargs):
+        raise Http404()
