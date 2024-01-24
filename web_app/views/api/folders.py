404307cc604795a52ef7425c0815df366b719e89
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-24 08:05:39 +0100

    refactor: search field

--- web_app/views/api/folders.py
+++ web_app/views/api/folders.py
@@ -5,12 +5,12 @@ from django.shortcuts import render
 import re
 from web_app.models import GoogleDrive, OneDrive
 from allauth.socialaccount.adapter import get_adapter
+from django.http import HttpResponseBadRequest  # Import HttpResponseBadRequest
 
 
 @login_required
 @require_POST
 def search_folder(request):
-    folders = None
     if request.method == 'POST':
         search_query = None
         search_type = None
@@ -23,9 +23,10 @@ def search_folder(request):
             elif re.match(type_pattern, key):
                 search_type = request.POST[key]
         folders = request.custom_user.get_folders(search_type, search_query)
-    return render(request,
-                  'private/space/create/components/folders_search_results.html',
-                  {'folders': folders, 'destination_type': search_type})
+        return render(request,
+                      'private/space/create/components/folders_search_results.html',
+                      {'folders': folders, 'destination_type': search_type})
+    return HttpResponseBadRequest()
 
 
 @login_required
@@ -34,14 +35,15 @@ def select_destination_type(request):
     if request.method == 'POST':
         custom_user = request.custom_user
         type_pattern = re.compile(r'.*destination_type_select.*')
+        provider_available = False
+        missing_provider = None
+        provider_type = None
 
         for key in request.POST.keys():
             if re.match(type_pattern, key):
                 provider_type = request.POST[key]
                 break
 
-        provider_available = False
-        missing_provider = None
         if provider_type == GoogleDrive.TAG:
             if custom_user.google_account:
                 provider_available = True
@@ -55,9 +57,11 @@ def select_destination_type(request):
             else:
                 adapter = get_adapter()
                 missing_provider = adapter.get_provider(request, OneDrive.PROVIDER_ID)
+        else:
+            return HttpResponseBadRequest()
 
         return render(request,
                       'private/space/create/components/destination_search.html',
                       {'provider_available': provider_available,
                        'missing_provider': missing_provider, })
-    raise Http404()
+    return HttpResponseBadRequest()
