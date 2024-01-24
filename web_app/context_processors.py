94288579f46cfdc8ee86aa1f4a21bd25bd12133d
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-24 17:48:40 +0100

    fix: currency

--- web_app/context_processors.py
+++ web_app/context_processors.py
@@ -1,4 +1,6 @@
+from django.contrib.humanize.templatetags.humanize import intcomma
 from djstripe.models import Product
+from djstripe.utils import get_friendly_currency_amount, CURRENCY_SIGILS
 
 import config
 from django.conf import settings
@@ -16,4 +18,9 @@ def custom_context(request):
     #context['config_data'] = config.get_js_config()
     context['contact_email'] = settings.CONTACT_EMAIL
     context['pro_product'] = Product.objects.filter(name="Pro").first()
+    unit_amount = (context['pro_product'].default_price.unit_amount or 0) / 100
+    sigil = CURRENCY_SIGILS.get(context['pro_product'].default_price.currency.upper(), "")
+    amount_two_decimals = f"{unit_amount:.2f}"
+    formatted_price = f"{sigil}{intcomma(amount_two_decimals)}"
+    context['pro_product_formatted_price'] = formatted_price
     return context
