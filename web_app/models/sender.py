8b7e5eae39299b9bcf92809f960c1447daf59988
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-28 11:17:00 +0100

    chore: remove repeated code

--- web_app/models/sender.py
+++ web_app/models/sender.py
@@ -39,6 +39,12 @@ class Sender(BaseModel, ActiveModel):
         self.notification_task = task
         self.save()
 
+    @property
+    def full_space_link(self):
+        return settings.BASE_URL + reverse('sender_space_detail_private', kwargs={
+                                                        'space_uuid': self.space.uuid,
+                                                        'sender_uuid': self.uuid
+                                                        })
     def notify_deadline(self):
         from web_app.models import UploadRequest
         if self.is_active:
@@ -49,10 +55,7 @@ class Sender(BaseModel, ActiveModel):
                 'upload_requests': upload_requests,
                 'homepage_link': settings.BASE_URL,
                 'logo_link': settings.BASE_URL + static('images/logo.png'),
-                'space_link': settings.BASE_URL + reverse('sender_space_detail_private', kwargs={
-                                                        'space_uuid': self.space.uuid,
-                                                        'sender_uuid': self.uuid
-                                                        })
+                'space_link': self.full_space_link
                 
             }
             email_html = render_to_string('emails/deadline_notification.html', context)
@@ -90,10 +93,7 @@ class Sender(BaseModel, ActiveModel):
                 'upload_requests': upload_requests,
                 'homepage_link': settings.BASE_URL,
                 'logo_link': settings.BASE_URL + static('images/logo.png'),
-                'space_link': settings.BASE_URL + reverse('sender_space_detail_private', kwargs={
-                                                        'space_uuid': self.space.uuid,
-                                                        'sender_uuid': self.uuid
-                                                        })
+                'space_link': self.full_space_link
                 
             }
         calendar_url, ics_content = self.space.get_deadline_url_ics(self)
