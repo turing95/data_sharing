f64101df5050d2e3c7b9dba1aad7291561ac775c
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-30 11:39:10 +0100

    chore: landing

--- static/src/js/home/index.js
+++ static/src/js/home/index.js
@@ -1,8 +1,6 @@
 import { initSmoothScrolling, adjustScrollPosition } from './smoothScrolling.js';
-import { initDriveProviderSwitch } from './switchDriveProvider.js';
 
 document.addEventListener('DOMContentLoaded', function() {
         initSmoothScrolling();
         adjustScrollPosition()
-        initDriveProviderSwitch() 
 });
