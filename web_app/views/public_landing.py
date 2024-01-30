b907c41f7538a5588e3d2b72772e5f35a4dd72cc
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-30 16:08:33 +0100

    feat: redirect homepage to spaces when logged in

--- web_app/views/public_landing.py
+++ web_app/views/public_landing.py
@@ -1,12 +1,18 @@
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 from web_app.mixins import SubscriptionMixin
 
 
-class PublicLandingView(SubscriptionMixin,TemplateView):
+class PublicLandingView(SubscriptionMixin, TemplateView):
     template_name = 'public/public_landing.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('spaces'))
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['generic_home'] = True
         return context
-
