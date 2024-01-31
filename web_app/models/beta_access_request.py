f095ee416d3bfa739b1cc1d9e72594cf37730e9b
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-31 09:58:15 +0100

    feat: forward to mail beta access request

--- web_app/models/beta_access_request.py
+++ web_app/models/beta_access_request.py
@@ -1,14 +1,46 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives, get_connection
 from django.db import models
 from web_app.models import BaseModel
 
 
 class BetaAccessRequest(BaseModel):
-
     user_name = models.CharField(max_length=50)
     user_email = models.CharField(max_length=50)
-    industry = models.CharField(max_length=50,null=True, blank=True)
-    country =  models.CharField(max_length=50,null=True, blank=True)
-    company =  models.CharField(max_length=50,null=True, blank=True)
-    user_role =  models.CharField(max_length=50,null=True, blank=True)
+    industry = models.CharField(max_length=50, null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+    company = models.CharField(max_length=50, null=True, blank=True)
+    user_role = models.CharField(max_length=50, null=True, blank=True)
     intended_use = models.TextField(null=True, blank=True)
-    first_touchpoint =  models.CharField(max_length=150,null=True, blank=True)
+    first_touchpoint = models.CharField(max_length=150, null=True, blank=True)
+
+    def notify(self):
+        txt = f"""
+        New beta access request from {self.user_name} ({self.user_email})
+        Industry: {self.industry}
+        Country: {self.country}
+        Company: {self.company}
+        User role: {self.user_role}
+        Intended use: {self.intended_use}
+        First touchpoint: {self.first_touchpoint}
+        """
+
+        with get_connection(
+                host=settings.RESEND_SMTP_HOST,
+                port=settings.RESEND_SMTP_PORT,
+                username=settings.RESEND_SMTP_USERNAME,
+                password=settings.RESEND_API_KEY,
+                use_tls=True,
+        ) as connection:
+            msg = EmailMultiAlternatives(
+                subject=f'New beta access request from {self.user_name} ({self.user_email})',
+                body=txt,
+                from_email=settings.NO_REPLY_EMAIL,
+                to='beta@kezyy.com',
+                reply_to=[settings.NO_REPLY_EMAIL],
+                connection=connection,
+                headers={'Return-Path': settings.NO_REPLY_EMAIL}
+            )
+            msg.attach_alternative(txt, 'text/html')
+            msg.send()
+        return True
--- web_app/tasks/notifications.py
+++ web_app/tasks/notifications.py
@@ -4,7 +4,7 @@ from web_app import celery_app as app
 
 
 @app.task(autoretry_for=(smtplib.SMTPServerDisconnected,),
-          retry_kwargs={'max_retries': 5},default_retry_delay= 5)
+          retry_kwargs={'max_retries': 5}, default_retry_delay=5)
 def notify_invitation(sender_pk):
     from web_app.models import Sender
     sender = Sender.objects.get(pk=sender_pk)
@@ -12,8 +12,16 @@ def notify_invitation(sender_pk):
 
 
 @app.task(autoretry_for=(smtplib.SMTPServerDisconnected,),
-          retry_kwargs={'max_retries': 5},default_retry_delay= 5)
+          retry_kwargs={'max_retries': 5}, default_retry_delay=5)
 def notify_deadline(sender_pk):
     from web_app.models import Sender
     sender = Sender.objects.get(pk=sender_pk)
     sender.notify_deadline()
+
+
+@app.task(autoretry_for=(smtplib.SMTPServerDisconnected,),
+          retry_kwargs={'max_retries': 5}, default_retry_delay=5)
+def notify_beta_access_request(req_pk):
+    from web_app.models import BetaAccessRequest
+    req = BetaAccessRequest.objects.get(pk=req_pk)
+    req.notify()
