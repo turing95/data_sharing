1bbc30d4a3f3188247dff1c821de95e4437eca22
marcoturetta <marco.turetta@blinklastmile.com>
2024-01-31 10:26:10 +0100

    feat: legal pages management

---
+++ web_app/views/legal/cookie_policy.py
@@ -0,0 +1,7 @@
+from django.shortcuts import redirect
+from django.views.decorators.http import require_GET
+
+
+@require_GET
+def cookie_policy(request):
+    return redirect('https://www.iubenda.com/privacy-policy/54001192/')
