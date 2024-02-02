b6002d2d842cdb32160a55145f4db01f0bddb529
marcoturetta <marco.turetta@blinklastmile.com>
2024-02-02 09:16:54 +0100

    fix: js

--- static/src/js/space/create/index.js
+++ static/src/js/space/create/index.js
@@ -16,36 +16,6 @@ document.addEventListener('click',function(event) {
 
 });
 
-//handle enter button behavior
-document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.querySelector('#space-form');
-    if (form) {
-        form.addEventListener('keydown', (e) => {
-            if (e.key === 'Enter' && e.target.matches('input:not([type="submit"]):not([type="button"]):not([type="hidden"]):not([class*="email-input"]), select')) {
-                e.preventDefault(); // Prevent form submission
-
-                const formInputs = Array.from(form.querySelectorAll('input:not([type="submit"]):not([type="button"]):not([type="hidden"]), select, textarea'));
-                const currentIndex = formInputs.indexOf(e.target);
-
-                if (currentIndex !== -1) {
-                    let nextIndex = currentIndex + 1;
-                    while (nextIndex < formInputs.length) {
-                        const nextInput = formInputs[nextIndex];
-                        if (!nextInput.disabled) { // add && !nextInput.readOnly to include read only fields
-                            nextInput.focus();
-                            if (document.activeElement === nextInput) {
-                                break; // Focus successfully moved
-                            }
-                        }
-                        nextIndex++;
-                    }
-                }
-            }
-        });
-    }
-});
-
-
 
 
 
