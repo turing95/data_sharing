59a9a92c1b19de49690e702a1bed7369867f6eca
marcoturetta <marco.turetta@blinklastmile.com>
2024-02-02 09:11:54 +0100

    fix: js clickoutside + refactor js

--- static/src/js/space/create/index.js
+++ static/src/js/space/create/index.js
@@ -1,6 +1,6 @@
 import {initRequestForms,toggleRename,handleTagDropdownChange,addFileTypeTag, toggleFileTypeRestrict,selectFolder } from "./request/index.js";
 import {initForm} from "./form.js";
-import {clickOutsideSearch} from "./eventHandlers.js";
+import {hideShowSearch} from "./eventHandlers.js";
 
 window.toggleRename = toggleRename;
 window.toggleFileTypeRestrict= toggleFileTypeRestrict
@@ -10,7 +10,10 @@ window.selectFolder = selectFolder;
 document.addEventListener('DOMContentLoaded', function() {
     initForm();
     initRequestForms();
-    document.addEventListener('click',clickOutsideSearch);
+});
+document.addEventListener('click',function(event) {
+    hideShowSearch(event);
+
 });
 
 //handle enter button behavior
