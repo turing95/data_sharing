81334eba1479259a6ec639e85f411e66bd33b457
marcoturetta <marco.turetta@blinklastmile.com>
2024-02-07 23:15:44 +0100

    refactor: user model

--- web_app/models/user.py
+++ web_app/models/user.py
@@ -7,7 +7,7 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from msal import ConfidentialClientApplication
 import requests
-import config
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -29,20 +29,6 @@ class User(AbstractUser):
             return None
 
     @property
-    def custom_google_account(self):
-        try:
-            return SocialAccount.objects.get(user=self, provider='custom_google')
-        except SocialAccount.DoesNotExist:
-            return None
-
-    @property
-    def custom_microsoft_account(self):
-        try:
-            return SocialAccount.objects.get(user=self, provider='custom_microsoft')
-        except SocialAccount.DoesNotExist:
-            return None
-
-    @property
     def microsoft_account(self):
         try:
             return SocialAccount.objects.get(user=self, provider='custom_microsoft')
@@ -52,38 +38,28 @@ class User(AbstractUser):
     def get_folders(self, destination_type, folder_name=None):
         from web_app.models import OneDrive, GoogleDrive
         if destination_type == GoogleDrive.TAG:
-            return self.get_google_drive_folders(folder_name)
+            return GoogleService(self.google_account).get_folders(folder_name)
         elif destination_type == OneDrive.TAG:
-            return self.get_one_drive_folders(folder_name)
+            return MicrosoftService(self.microsoft_account).get_folders(folder_name)
         else:
             return None
 
-    def get_one_drive_folders(self, folder_name=None):
-        token = self.microsoft_token
-        if not token:
-            return None  # or handle the error as required
-
-        headers = {
-            'Authorization': f'Bearer {token.token}',
-            'Content-Type': 'application/json'
-        }
-        url = "https://graph.microsoft.com/v1.0/me/drive/root/"
-        if folder_name:
-            url += f"search(q='{folder_name}')"
-        else:
-            url += 'children'
-
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            folders = [item for item in response.json().get('value', []) if 'folder' in item]
-            return folders
-        else:
-            return None  # or handle the error as required
-
     def refresh_google_token(self, token=None):
+        return GoogleService(self.google_account).refresh_token(token)
+
+    def refresh_microsoft_token(self, token=None):
+        return MicrosoftService(self.microsoft_account).refresh_token(token)
+
+
+
+class GoogleService:
+    def __init__(self, social_account):
+        self.social_account = social_account
+
+    def refresh_token(self, token=None):
         try:
             if token is None:
-                social_account = self.google_account
+                social_account = self.social_account
                 if social_account is not None:
                     token = SocialToken.objects.get(account=social_account)
             if arrow.get(token.expires_at) < arrow.utcnow():
@@ -91,8 +67,8 @@ class User(AbstractUser):
                     token=token.token,
                     refresh_token=token.token_secret,
                     token_uri='https://accounts.google.com/o/oauth2/token',
-                    client_id=config.GOOGLE_CLIENT_ID,
-                    client_secret=config.GOOGLE_CLIENT_SECRET
+                    client_id=settings.GOOGLE_CLIENT_ID,
+                    client_secret=settings.GOOGLE_CLIENT_SECRET
                 )
                 credentials.refresh(Request())
                 token.token = credentials.token
@@ -105,9 +81,48 @@ class User(AbstractUser):
             # or the token does not exist
             return None
 
-    def refresh_microsoft_token(self, token=None):
+    @property
+    def _credentials(self):
+        token = self.refresh_token()
+        if token:
+            return Credentials(
+                token=token.token,
+                refresh_token=token.token_secret,
+                token_uri='https://accounts.google.com/o/oauth2/token',
+                client_id=settings.GOOGLE_CLIENT_ID,
+                client_secret=settings.GOOGLE_CLIENT_SECRET
+            )
+        else:
+            return None
+
+    @property
+    def _service(self):
+        return build('drive', 'v3', credentials=self._credentials)
+
+    def get_folders(self, folder_name):
+        credentials = self._credentials
+        if not credentials:
+            return None
+        response = (
+            self._service.files()
+            .list(
+                q="mimeType='application/vnd.google-apps.folder' and name contains '" + folder_name + "'",
+                spaces="drive",
+                fields="files(id, name)",
+                supportsAllDrives=True
+            )
+            .execute()
+        )
+        return response.get('files', [])
+
+
+class MicrosoftService:
+    def __init__(self, social_account):
+        self.social_account = social_account
+
+    def refresh_token(self, token=None):
         if token is None:
-            social_account = self.microsoft_account
+            social_account = self.social_account
             if social_account is None:
                 return None
 
@@ -115,8 +130,8 @@ class User(AbstractUser):
         if arrow.get(token.expires_at) < arrow.utcnow():
             # Create a Confidential Client Application
             app = ConfidentialClientApplication(
-                config.AZURE_CLIENT_ID,
-                client_credential=config.AZURE_CLIENT_SECRET
+                settings.AZURE_CLIENT_ID,
+                client_credential=settings.AZURE_CLIENT_SECRET
             )
             result = app.acquire_token_by_refresh_token(
                 refresh_token=token.token_secret,
@@ -138,44 +153,33 @@ class User(AbstractUser):
                 return None
         return token
 
-    @property
-    def google_token(self):
-        return self.refresh_google_token()
+    def get_folders(self, folder_name=None):
+        token = self.refresh_token()
 
-    @property
-    def microsoft_token(self):
-        return self.refresh_microsoft_token()
-
-    @property
-    def google_credentials(self):
-        token = self.refresh_google_token()
-        if token:
-            return Credentials(
-                token=token.token,
-                refresh_token=token.token_secret,
-                token_uri='https://accounts.google.com/o/oauth2/token',
-                client_id=config.GOOGLE_CLIENT_ID,
-                client_secret=config.GOOGLE_CLIENT_SECRET
-            )
+        headers = {
+            'Authorization': f'Bearer {token.token}',
+            'Content-Type': 'application/json'
+        }
+        url = "https://graph.microsoft.com/v1.0/me/drive/root/"
+        if folder_name:
+            url += f"search(q='{folder_name}')"
         else:
-            return None
+            url += 'children'
 
-    @property
-    def google_service(self):
-        return build('drive', 'v3', credentials=self.google_credentials)
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            folders = [item for item in response.json().get('value', []) if 'folder' in item]
+            return folders
+        else:
+            return None  # or handle the error as required
 
-    def get_google_drive_folders(self, folder_name):
-        credentials = self.google_credentials
-        if not credentials:
-            return None
-        response = (
-            self.google_service.files()
-            .list(
-                q="mimeType='application/vnd.google-apps.folder' and name contains '" + folder_name + "'",
-                spaces="drive",
-                fields="files(id, name)",
-                supportsAllDrives=True
-            )
-            .execute()
-        )
-        return response.get('files', [])
+
+    def list_sites(self):
+        token = self.refresh_token()
+        url = "https://graph.microsoft.com/v1.0/sites/"
+        headers = {
+            'Authorization': f'Bearer {token.token}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        return response.json()
