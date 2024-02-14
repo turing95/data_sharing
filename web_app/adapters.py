647d975017963abeeadac4b7a3f30a4a5dc91fe7
marcoturetta <marco.turetta@blinklastmile.com>
2024-02-14 12:56:33 +0100

    feat: reconnection flow

--- web_app/adapters.py
+++ web_app/adapters.py
@@ -1,7 +1,9 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.models import SocialAccount
 from django.contrib import messages
 from django.core.exceptions import ValidationError
+from django.shortcuts import redirect
 from django.urls import reverse
 
 from web_app.models import Organization, SenderNotificationsSettings
@@ -20,6 +22,19 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
             messages.error(self.request, "You can not disconnect from your last account")
             raise ValidationError("Can not disconnect")
 
+    def pre_social_login(self, request, sociallogin):
+        # Assuming 'sociallogin' is the social login attempt being processed
+        # Check if the user is authenticated and the social account is not already linked
+        if request.user.is_authenticated:
+            existing_social_accounts = SocialAccount.objects.filter(provider=sociallogin.account.provider,
+                                                                    user=request.user)
+            if existing_social_accounts.exists():
+                # Here you would handle the case where the user already has a social account for this provider
+                # For example, you could redirect to a custom error page or display an error message
+                # This is a placeholder for how you might start a redirect; adjust as necessary for your application
+                provider = self.get_provider(request, sociallogin.account.provider)
+                messages.error(request, f"You already have an account with {provider.name}, reconnect or disconnect the existing one.")
+                return redirect(request.get_full_path())
     def save_user(self, request, user, form=None):
         # Call the super class's save_user to save the user model
         user = super().save_user(request, user, form)
