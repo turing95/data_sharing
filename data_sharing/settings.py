19623d2412fae1adfc1192213f054714094fe18e
marcoturetta <marco.turetta@blinklastmile.com>
2024-02-15 16:39:47 +0100

    fix: translatio ndeploy

--- data_sharing/settings.py
+++ data_sharing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 from config import *
 import os
 import sentry_sdk
