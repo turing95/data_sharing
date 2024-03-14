fe720f47b8ba2417806e9da440255e15ffbff4df
GiulioCantoro <giulio.cantoro@gmail.com>
2024-03-14 08:32:36 +0100

    feat: table request styiling. missing delete and duplicate conections

--- web_app/views/space/detail/requests.py
+++ web_app/views/space/detail/requests.py
@@ -1,14 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from web_app.mixins import SubscriptionMixin, SpaceMixin, SpaceSideBarMixin, SpaceTabMixin
 
 
 class SpaceRequestsView(LoginRequiredMixin, SubscriptionMixin, SpaceMixin, SpaceSideBarMixin, SpaceTabMixin,
-                        TemplateView):
+                        ListView):
     template_name = "private/space/detail/request/list.html"
+    paginate_by = 12
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['space_summary'] = True
         context['space_tab']['requests']['active'] = True
         return context
+    
+    def get_queryset(self):
+        return self.get_space().requests.all().order_by('created_at')
