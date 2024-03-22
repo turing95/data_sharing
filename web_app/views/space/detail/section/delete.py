25242582cb62aad35fc140af3dc0cf08634cd657
GiulioCantoro <giulio.cantoro@gmail.com>
2024-03-22 09:35:21 +0100

    fix

--- web_app/views/space/detail/section/delete.py
+++ web_app/views/space/detail/section/delete.py
@@ -8,8 +8,8 @@ from django.http import HttpResponse
 @login_required
 @require_POST
 def section_delete(request, space_section_uuid):
-    input_request = get_object_or_404(SpaceSection, pk=space_section_uuid)
-    input_request.delete()  
+    section = get_object_or_404(SpaceSection, pk=space_section_uuid)
+    section.delete()  
     response = HttpResponse()
     response['HX-Trigger'] = 'update_order'
     return response
