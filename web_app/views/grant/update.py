7ba481a838b35e8a7947d99e312e8e4862e24347
marcoturetta <marco.turetta@blinklastmile.com>
2024-04-02 10:08:49 +0200

    fix

--- web_app/views/grant/update.py
+++ web_app/views/grant/update.py
@@ -6,6 +6,35 @@ from django.views.decorators.http import require_POST
 
 from web_app.models import Grant
 
+from django.views.generic import FormView
+from web_app.mixins import SubscriptionMixin, GrantSideBarMixin, GrantMixin, GrantTabMixin
+from web_app.forms import GrantForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class GrantEditView(LoginRequiredMixin, SubscriptionMixin, GrantSideBarMixin, GrantTabMixin, GrantMixin, FormView):
+    template_name = "private/grant/edit.html"
+    form_class = GrantForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['grant_tab']['edit']['active'] = True
+        return context
+
+    # def get_success_url(self):
+    #     return reverse('companies', kwargs={'organization_uuid': self.get_company().organization.pk})
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.get_grant()
+        kwargs['organization'] = self.get_organization()
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
 @login_required
 @require_POST
 def grant_update_name(request, grant_uuid):
